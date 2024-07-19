e955cc247cfd3bbe31cb6185c4bd1ba86f9a2aa5
Guilherme0112 <guimendesmen124@gmail.com>
2024-07-19 10:58:17 -0300

    Página Inicial

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('links')
 <link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/media-query.css">
 @endsection
 
 @section('title', 'Início')
@@ -12,7 +13,19 @@
 <a href="{{ route('pages-sobre') }}" class="link-header">Quem Somos?</a>
 @endsection
 @section('main')
-
+    <div class="background">
+        <div class="text">
+            <h2 class="title-text">Por que ir ao Dentista?</h2>
+            <p class="text-text">
+                Manter a saúde bucal em dia é fundamental para o bem-estar geral do nosso corpo. Uma das maneiras mais eficazes de garantir isso é através de visitas regulares ao dentista. O dentista não só cuida da saúde dos nossos dentes, mas também da saúde da nossa gengiva e da nossa boca como um todo. 
+ 
+                Ir ao dentista regularmente ajuda a prevenir problemas bucais, como cáries, gengivite e doenças periodontais. Além disso, o dentista pode identificar precocemente qualquer problema e tratá-lo antes que se agrave, o que pode resultar em tratamentos mais simples e menos invasivos.
+            </p>
+        </div>
+        <div class="div-btn">
+            <button class="btn btn-primary btn-back">Fazer meu plano</button>
+        </div>
+    </div>
 @endsection
 @section('footer')
 
